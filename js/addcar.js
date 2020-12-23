44233442fd21e270cf295def4197c5475a91cee0
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2020-12-24 00:21:58 +0900

    Admin 画面作成

--- js/addcar.js
+++ js/addcar.js
@@ -1,29 +1,43 @@
 $(function(){
 
-    // var carImageNum = 1;
+  var carImageNum = 1;
 
-    $("#carsubmit").on('click',function(){
-        let exhibitionNumber = $('#ExhibitionNumber').val();
-        let bodyNumber = $('#BodyNumber').val();
-        let grade = $('#Grade').val();
-        console.log(exhibitionNumber);
-    });
+  $("#carsubmit").on('click',function(){
+    let exhibitionNumber = $('#ExhibitionNumber').val();
+  	let bodyNumber = $('#BodyNumber').val();
+    let grade = $('#Grade').val();
+    console.log(exhibitionNumber);
+	});
 
-    $('document').on('change', 'input[type="file"]', function(){
-        let fileName = $('#InputFile').val();
-        $('.custom-file-label').val(fileName);
+	$('#AddImage').on('click', function(){
 
-        $('#CarImage')
+		let inputFileID = 'InputFile' + carImageNum;
+		$('#CarImage').append(
+			$('<div class="input-group">').append(
+				$('<div class="custom-file">').append(
+					'<input type="file" class="custom-file-input" id="' + inputFileID + '">',).append('<label class="custom-file-label" for="' + inputFileID + '">Choose file')
+			)
+		);
+		carImageNum++;
+	});
 
-        /*
-        $('#CarImage').append(
-            '<div class="input-group">',
-            '<div class="custom-file">',
-            '<input type="file" class="custom-file-input" id="InputFile">',
-            '<label class="custom-file-label" for="InputFile">Choose file</label>',
-            '</div>',
-            '</div>'
-        )
-        */
-    });
+	/*
+	$('.custom-file-input').each(function(index, element){
+		$('body').on('change', '',function(){
+			let inputImage = $(element).prop('files')[0].name;
+			console.log('file change', inputImage);
+			$('.custom-file-label').eq(index).text(inputImage);
+			console.log('label text change');
+		});
+	});
+	*/
+
+	$('.custom-file-input').each(function(index, element){
+		$('body').on('change', element, function(){
+			let inputImage = $(element).prop('files')[0].name;
+			console.log('file change', inputImage);
+			$('.custom-file-label').eq(index).text(inputImage);
+			console.log('label text change');
+		});
+	});
 });
