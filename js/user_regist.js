6bc23ccc0bb288cb0f4fa37709ffe1b464c1189c
Narwhal-cloud <60862634+Narwhal-cloud@users.noreply.github.com>
2021-01-18 11:57:37 +0900

    user_login html

---
+++ js/user_regist.js
@@ -0,0 +1,23 @@
+$("#regist_info").on("submit", function(event) {
+    event.preventDefault();
+    var send_data = {
+        name: $('#regist_info input[name="InputUserName"]').val(),
+        name: $('#regist_info input[name="InputMail"]').val(),
+        name: $('#regist_info input[name="InputPassword1"]').val(),
+        name: $('#regist_info input[name="InputPassword2"]').val(),
+        name: $('#regist_info input[name="zip21"]').val(),
+        name: $('#regist_info input[name="zip22"]').val(),
+        name: $('#regist_info input[name="adr21"]').val(),
+        name: $('#regist_info input[name="InputTel"]').val(),
+    };
+    $.ajax({
+        type: "post",
+        url: "/",
+        data: send_data
+    }).done(function(results) {
+        console.log(results);
+        alert("情報を登録しました。")
+    }).fail(function(xhr, textStatus, errorThrown) {
+        console.log("ajax通信に失敗しました")
+    }).always(function(xhr) {});
+});
