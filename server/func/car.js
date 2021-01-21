258fbba8294fb2353a2bb89abd24b9f92db78437
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2021-01-21 11:27:13 +0900

    処理分け module 追加

---
+++ server/func/car.js
@@ -0,0 +1,9 @@
+var router = require("express").Router();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
+
+
+
+module.exports = router;
---
+++ server/func/user.js
@@ -0,0 +1,8 @@
+var router = require("express").Router();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
+
+
+module.exports = router;
