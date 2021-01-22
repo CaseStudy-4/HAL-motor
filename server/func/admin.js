7b74078a417b336a71dfb1d573b4c6978a6b6710
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2021-01-22 12:01:56 +0900

    auction.js put 追加

--- server/func/admin.js
+++ server/func/admin.js
@@ -63,8 +63,6 @@ router.post('/login', passport.authenticate('local'),function(req, res){
 */
 router.use(bodyParser.json());
 router.post('/', async function(req,res) {
-	console.log('post request!!');
-	console.log(req.body);
 
 	let connection
 	try {
@@ -200,6 +198,7 @@ router.get('/Email/:e-mail', function(req, res) {
 *	[PUT]: Update
 *	param: adminID
 */
+/*
 router.put('/:adminID', function(req, res){
 	mysqlconnection.query(
 		'UPDATE employee_info SET  WHERE employee_id = ?', [req.params.adminID],
@@ -215,6 +214,7 @@ router.put('/:adminID', function(req, res){
 		}
 	)
 });
+*/
 
 /*
 *	[DELETE]: Delete
